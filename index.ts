18eae1c7beec0aa9b4e1c54624a277ae000d04bb
adhemukhlis <adhemukhlis@gmail.com>
2026-01-20 15:16:57 +0700

    move `index.ts` to root

---
+++ index.ts
@@ -0,0 +1,3 @@
+export { encrypt, decrypt } from './src/modules/crypto'
+export { sanitizeString, kebabLower, snakeLower } from './src/modules/string'
+export { decodePolyline, encodePolyline } from './src/modules/map'
--- src/index.ts
+++
@@ -1,3 +0,0 @@
-export { encrypt, decrypt } from './modules/crypto'
-export { sanitizeString, kebabLower, snakeLower } from './modules/string'
-export { decodePolyline, encodePolyline } from './modules/map'
