2eefe715ce9d723447a219b6bc0096c2ac0ae4a8
adhemukhlis <adhemukhlis@gmail.com>
2026-01-20 16:51:02 +0700

    adding type export

--- index.ts
+++ index.ts
@@ -1,3 +1,3 @@
 export { encrypt, decrypt } from './src/modules/crypto'
 export { sanitizeString, kebabLower, snakeLower } from './src/modules/string'
-export { decodePolyline, encodePolyline, calculateHeading } from './src/modules/map'
+export { type Coordinate, type CoordinateObject, decodePolyline, encodePolyline, calculateHeading } from './src/modules/map'
--- src/modules/map/index.ts
+++ src/modules/map/index.ts
@@ -1,3 +1,6 @@
 export { default as decodePolyline } from './decode-polyline'
 export { default as encodePolyline } from './encode-polyline'
 export { default as calculateHeading } from './calculate-heading'
+
+// types
+export type { Coordinate, CoordinateObject } from './types'
