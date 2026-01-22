68c682442188659509d456aef268ca73a35a39a8
adhemukhlis <adhemukhlis@gmail.com>
2026-01-22 16:23:55 +0700

    add `FlattenResult` and `FlattenResultStrict` types

--- src/modules/array/types.ts
+++ src/modules/array/types.ts
@@ -1,3 +1,5 @@
 export type FlattenData = Record<string, any>
-export type Flatten = <T extends FlattenData, K extends keyof T>(header: readonly K[], data: readonly T[]) => { h: readonly K[] } & Record<number, any[]>
+export type FlattenResultStrict<K extends PropertyKey> = { h: readonly K[] | string[] } & Record<number, any[]>
+export type FlattenResult = { h: readonly string[] } & Record<number, any[]>
+export type Flatten = <T extends FlattenData, K extends keyof T>(header: readonly K[], data: readonly T[]) => FlattenResultStrict<K>
 export type Unflatten = <K extends PropertyKey>(payload: any) => Array<Record<K, unknown>>
